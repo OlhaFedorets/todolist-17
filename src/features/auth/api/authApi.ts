a1403d011776d0ae4074051ccac7cb3402ea8a4b
OlhaFedorets <fedoretsolha@gmail.com>
2025-05-20 16:50:18 +0300

    change authApi

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,11 +1,16 @@
 import { instance } from "@/common/instance"
 import type { BaseResponse } from "@/common/types"
-import { Inputs } from "@/features/auth/lib/schemas"
+import { LoginInputs } from "@/features/auth/lib/schemas"
+import {MeResponse} from "@/features/auth/api/authApi.type.ts";
 
 export const authApi = {
-  login(payload: Inputs) {
-    return instance.post<BaseResponse<{ userId: number, token: string }>>(`/auth/login`, payload)
+  login(args: LoginInputs) {
+    return instance.post<BaseResponse<{ userId: number, token: string }>>(`/auth/login`, args)
+  },
+  logout() {
+    return instance.delete<BaseResponse>(`/auth/login`)
+  },
+  me() {
+    return instance.get<BaseResponse<MeResponse>>(`/auth/me`)
   }
 }
-
-
