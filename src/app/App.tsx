8059e747bbaa05c46f1dae9840edc9192e3abfe3
OlhaFedorets <fedoretsolha@gmail.com>
2025-05-21 16:31:58 +0300

    change app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,45 @@
 import "./App.css"
-import { selectThemeMode } from "@/app/app-slice"
-import { ErrorSnackbar, Header } from "@/common/components"
-import { useAppSelector } from "@/common/hooks"
-import { Routing } from "@/common/routing"
-import { getTheme } from "@/common/theme"
+import {selectThemeMode} from "@/app/app-slice"
+import {ErrorSnackbar, Header} from "@/common/components"
+import {useAppDispatch, useAppSelector} from "@/common/hooks"
+import {Routing} from "@/common/routing"
+import {getTheme} from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
-import { ThemeProvider } from "@mui/material/styles"
+import {ThemeProvider} from "@mui/material/styles"
+import {useEffect, useState} from "react";
+import {meTC} from "@/features/auth/model/auth-slice.ts";
 
 export const App = () => {
-  const themeMode = useAppSelector(selectThemeMode)
 
-  const theme = getTheme(themeMode)
+    const [isInit, setIsInit] = useState(false)
 
-  return (
-    <ThemeProvider theme={theme}>
-      <div className={"app"}>
-        <CssBaseline />
-        <Header />
-        <Routing />
-        <ErrorSnackbar />
-      </div>
-    </ThemeProvider>
-  )
+    const themeMode = useAppSelector(selectThemeMode)
+
+    const theme = getTheme(themeMode)
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(meTC()).finally(() => {
+            setIsInit(true)
+        })
+        dispatch(meTC()).unwrap().then(() => {
+            setIsInit(true)
+        })
+    })
+
+    if (!isInit) {
+        return <h1>Loading...</h1>;
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <div className={"app"}>
+                <CssBaseline/>
+                <Header/>
+                <Routing/>
+                <ErrorSnackbar/>
+            </div>
+        </ThemeProvider>
+    )
 }
