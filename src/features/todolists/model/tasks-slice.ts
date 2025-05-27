a2f78cf93f07bb2d86ff249f7617db7c700e1f53
OlhaFedorets <fedoretsolha@gmail.com>
2025-05-27 14:33:21 +0300

    change tasks-slice.ts

--- src/features/todolists/model/tasks-slice.ts
+++ src/features/todolists/model/tasks-slice.ts
@@ -1,10 +1,11 @@
-import { setAppStatusAC } from "@/app/app-slice"
-import type { RootState } from "@/app/store"
-import { ResultCode } from "@/common/enums"
-import { createAppSlice, handleServerAppError, handleServerNetworkError } from "@/common/utils"
-import { tasksApi } from "@/features/todolists/api/tasksApi"
-import { type DomainTask, DomainTaskSchema, type UpdateTaskModel } from "@/features/todolists/api/tasksApi.types"
-import { createTodolistTC, deleteTodolistTC } from "./todolists-slice"
+import {setAppStatusAC} from "@/app/app-slice"
+import type {RootState} from "@/app/store"
+import {ResultCode} from "@/common/enums"
+import {createAppSlice, handleServerAppError, handleServerNetworkError} from "@/common/utils"
+import {tasksApi} from "@/features/todolists/api/tasksApi"
+import {type DomainTask, DomainTaskSchema, type UpdateTaskModel} from "@/features/todolists/api/tasksApi.types"
+import {createTodolistTC, deleteTodolistTC} from "./todolists-slice"
+import {clearDataAC} from "@/common/actions";
 
 export const tasksSlice = createAppSlice({
   name: "tasks",
@@ -20,6 +21,9 @@ export const tasksSlice = createAppSlice({
       .addCase(deleteTodolistTC.fulfilled, (state, action) => {
         delete state[action.payload.id]
       })
+        .addCase(clearDataAC, () => {
+          return {}
+        })
   },
   reducers: (create) => ({
     fetchTasksTC: create.asyncThunk(
